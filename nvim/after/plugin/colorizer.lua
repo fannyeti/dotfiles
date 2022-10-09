dea9bc0189589b26e163425c6ce1edc1154543c1
xfanny <37284373+fannyeti@users.noreply.github.com>
2022-10-09 19:05:11 +0700

    Delete nvim directory

--- nvim/after/plugin/colorizer.lua
+++
@@ -1,8 +0,0 @@
-local status_colorizer_ok, colorizer = pcall(require, "colorizer")
-if not status_colorizer_ok then
-	return
-end
-
-colorizer.setup({
-	"*",
-})
--- nvim/after/plugin/hop.lua
+++
@@ -1,6 +0,0 @@
-local status_hop_ok, hop = pcall(require, "hop")
-if not status_hop_ok then
-	return
-end
-
-hop.setup()
